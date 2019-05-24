1d7b76b5a320b9a39560276964c3cdb546004328
Naoto Nakazawa <naoto_nakazawa@NaotonoMacBook-Pro.local>
2019-05-24 13:24:51 +0900

    Commit

--- Naoto/Part03/ex03.py
+++ Naoto/Part03/ex03.py
@@ -36,11 +36,12 @@ def Viterbi_algorithm(uni_prob: {}, test_path: str, test_ans_path: str):
         best_score = defaultdict(lambda: 0)
         lam_1 = 0.95
         lam_unk = 1-lam_1
+        N = 100000
         for line in f:
-            # line = line.rstrip().split()  # 形態素解析済の日本語
-            line = list(line.rstrip())  # 英語
-            best_edge[0] = "NULL"
-            best_score[0] = 0
+            line = line.rstrip().split()  # 形態素解析済の日本語
+            # line = list(line.rstrip())  # 英語
+            best_edge = [None] * (len(line)+1)
+            best_score = [0] * (len(line)+1)
             for word_end in range(1, len(line) + 1):
                 best_score[word_end] = math.pow(10, 10)
                 for word_begin in range(word_end):
@@ -49,15 +50,15 @@ def Viterbi_algorithm(uni_prob: {}, test_path: str, test_ans_path: str):
                         word = word + i
                     if not(uni_prob[word] != 0 or len(word) == 1):
                         continue
-                    prob = lam_1*uni_prob[word] + lam_unk/len(word)
+                    prob = lam_1*uni_prob[word] + lam_unk/N
                     my_score = best_score[word_begin] - math.log(prob)
                     if not(my_score < best_score[word_end]):
                         continue
                     best_score[word_end] = my_score
                     best_edge[word_end] = (word_begin, word_end)
             words = []
-            next_edge = best_edge[len(best_edge) - 1]
-            while next_edge != "NULL":
+            next_edge = best_edge[-1]
+            while next_edge != None:
                 word = line[next_edge[0]:next_edge[1]]
                 words.append("".join(word))
                 next_edge = best_edge[next_edge[0]]
@@ -67,14 +68,14 @@ def Viterbi_algorithm(uni_prob: {}, test_path: str, test_ans_path: str):
 
 if __name__ == "__main__":
     #テスト
-    model_path = "04-model.txt"
-    test_path = "04-input.txt"
-    test_ans_path = "04-output.txt"
+    # model_path = "04-model.txt"
+    # test_path = "04-input.txt"
+    # test_ans_path = "04-output.txt"
     #本番
-    # train_path = "wiki-ja-train.word"
-    # model_path = "model.txt"
-    # test_path = "wiki-ja-test.word"
-    # test_ans_path = "wiki-ja-test-ans.txt"
-    # write_model(train_path, model_path)
+    train_path = "wiki-ja-train.word"
+    model_path = "model.txt"
+    test_path = "wiki-ja-test.word"
+    test_ans_path = "wiki-ja-test-ans.txt"
+    write_model(train_path, model_path)
     uni_prob = load_model(model_path)
     Viterbi_algorithm(uni_prob, test_path, test_ans_path)
