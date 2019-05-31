11c3b272569822f7e2bd6a0a662f8f083246ea7c
A. Akechi <smithakihide1992@gmail.com>
2019-05-31 11:46:44 +0900

    by Shibata, added comments.

--- hShibata/tutorial04/hmm.py
+++ hShibata/tutorial04/hmm.py
@@ -8,6 +8,7 @@ import sys
 import nGram
 
 
+# a class contains count of words, probability, and smoothing value (weight). this class will be constructed recursively to denote conditional distribution.
 class c_PE:
     def __init__(self, weight):
         self.count = 0
@@ -15,14 +16,16 @@ class c_PE:
         self.weight = weight
         self.pRand = defaultdict(lambda: c_PE(weight))
 
-
-gPE_given_pos = c_PE(0.95)
-gN = 10**6
-gPosSet = set()
-
+# grobal variables.
+gPE_given_pos = c_PE(0.95) # this value in constructor denotes the smoothing value of uni-gram.
+gN = 10**6 # assumed number of vocabulary in the real world.
+gPosSet = set() 
 
 def train(path):
-    strPos = ""
+    # this function trains the model, in which conditional probability of the state of word given the state of Part of Speech (POS), and transition probability of hidden units are estimated.
+
+    # path: file path to a training data.
+    strPos = "" # variable for storing pos tagging data to train 2-gram of Hidden units.
     with open(path, "r") as f:
         for line in f:
             line = line.strip()
@@ -33,15 +36,15 @@ def train(path):
                 gPE_given_pos.count += 1
                 gPE_given_pos.pRand[pos].count += 1
                 gPE_given_pos.pRand[pos].pRand[word].count += 1
-                gPosSet.add(pos)
+                gPosSet.add(pos) # add also to a state set of POS tags. 
 
     # build a bi-gram model for the POS Markov chain.
     with open("tempPOS.word", "w") as f:
         print(strPos[:-1], file=f)
 
-    nGram.mnGram = nGram.c_nGram(1)  # no smoothing
-    nGram.gN = 2  # bi-gram.
-    nGram.Train("tempPOS.word")
+    nGram.mnGram = nGram.c_nGram(1)  # 1 means no smoothing method is used.
+    nGram.gN = 2  # 2 means bi-gram.
+    nGram.Train("tempPOS.word") # data set file of tag data extracted in the above.
 
     # build a uni-gram model for the visible variable probability.
     for pos, gvn_pos in gPE_given_pos.pRand.items():
@@ -50,8 +53,9 @@ def train(path):
 
 
 def probW(word, pos):
+    # this function estimate the conditional probability of a word given a POS value.
     if pos not in gPE_given_pos.pRand:
-        raise Exception("unknown pos was found. this model is not supported.")
+        raise Exception("unknown pos was found. this model is not supported.") # should not occur.
 
     gvn_pos = gPE_given_pos.pRand[pos]
     weight = gPE_given_pos.weight
@@ -63,17 +67,23 @@ def probW(word, pos):
 
 
 def ViterbiDivision(path):
+    # this function runs Viterbi algorithm on the given graph, which defined as Graph in the following.
     with open(path, "r") as f:
         oStr = ""
         for line in f:
             line = line.strip()
             words = ["<s>"] + line.split(" ") + ["</s>"]
-            Graph = {}
-            # create a graph
+            Graph = {} # graph Viterbi on which algorithm will run.
+
+            # create a graph for a sentence.
             for i in range(1, len(words)):
+
+                # restrict the state set of the start and end of this sentence to "<s>" and "</s>" respectively.
+
+                # 1 means previous state, which is denoted using nodes of a graph. 2 means the current state.
                 if i == 1:
                     posSet1 = set({"<s>"})
-                    posSet2 = gPosSet - set({"<s>","</s>"})
+                    posSet2 = gPosSet - set({"<s>","</s>"}) # remove the end and start tags from an ordinal state set, which are neightier the start nor the end.
                 elif i == len(words)-1:
                     posSet1 = gPosSet - set({"<s>","</s>"})
                     posSet2 = set({"</s>"})
@@ -81,18 +91,18 @@ def ViterbiDivision(path):
                     posSet1 = gPosSet - set({"<s>","</s>"})
                     posSet2 = gPosSet - set({"<s>","</s>"})
 
-                word = words[i]
-
+                word = words[i] # get a word for index i.
                 for pos1 in posSet1:
                     tG = {}
                     for pos2 in posSet2:
+                        # estimate a log likelihood on the posterior distribution of POS tags, given the word.
                         ln = - math.log(nGram.probWord([pos1,pos2])+0.1**100) - math.log(probW(word, pos2))
 
-                        tG[str(i) + ":" +  pos2] = ln
+                        tG[str(i) + ":" +  pos2] = ln # make an index to be able to represent whole nodes enough.
 
                     Graph[str(i-1) + ":" +  pos1] = tG
 
-            # Excute Viterbi algorithm.
+            # Run Viterbi algorithm.
             best_score = {}
             best_edge = {}
             best_score[str(0) + ":" + "<s>"] = 0
@@ -108,27 +118,29 @@ def ViterbiDivision(path):
                     posSet2 = gPosSet - set({"<s>","</s>"})
 
                 for pos2 in posSet2:
-                    i2 = str(i) + ":" + pos2
-                    best_score[i2] = math.inf
+                    i2 = str(i) + ":" + pos2 # reconstruct the index corresponding to the above ones.
+                    best_score[i2] = math.inf # initialize with the highest cost.
                     for pos1 in posSet1:
                         i1 = str(i-1)  + ":" +  pos1
                         score = best_score[i1] + Graph[i1][i2]
                         if score < best_score[i2]:
                             best_score[i2] = score
                             best_edge[i2] = i1
-            poses = []
+
+            poses = [] # this variable will contain the sequence of POS tags which show the highest likelihood given the sequence of words.
+
             i1 = str(len(words)-1) + ":" + "</s>"
             while True:
-                poses.append(i1.split(":")[1])
+                poses.append(i1.split(":")[1]) # retrive tags from the index.
                 i2 = best_edge[i1]
                 i1 = i2
                 if "<s>" in i1:
-                    poses.append(i1.split(":")[1])
                     break
 
             poses.reverse()
-            poses.remove("<s>")
-            poses.remove("</s>")
+
+            # we have to make some addjustmet to evaluate this model using gradepos.pl
+            poses.remove("<s>") 
             print(poses)
             for i in range(0,len(poses)):
                 oStr = oStr + poses[i]
