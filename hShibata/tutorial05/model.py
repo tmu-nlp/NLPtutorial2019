b53bdcc1a52f31058266f90473d2c8ab499d02c8
A. Akechi <smithakihide1992@gmail.com>
2019-06-06 17:43:00 +0900

    by hSHibata.

---
+++ hShibata/tutorial05/model.py
@@ -0,0 +1,15 @@
+import os
+from collections import defaultdict
+import sys
+import math
+import json
+import shutil
+import subprocess
+import nGram
+
+if __name__ == "__main__":
+    a = nGram
+    b = nGram
+    b.gN = 2
+    print(a.gN) 
+    print(b.gN) 
